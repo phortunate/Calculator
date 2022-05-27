43f8a20962718d660e2d61c974254a0181c68f47
Stephen Tatum <stephenanthonytatum@gmail.com>
2022-05-26 20:08:22 -0500

    Add button clicks to update display

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const input = document.querySelector(".input");
+const btns = Array.from(document.querySelectorAll("button"));
 function add(a, b) {
     return a + b;
 }
@@ -18,3 +20,19 @@ function operate(operator, a, b) {
     return operator(a , b);
 }
 
+function writeToDisplay(number) {
+    let display = input.textContent;
+    if (number !== "." && display === "0") clearDisplay();
+    if (number === "." && display.includes("."))  number = "";
+
+    input.textContent += number;
+}
+
+function clearDisplay() {
+    input.textContent = "";
+}
+
+btns.forEach( button => button.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btnr2")) writeToDisplay(e.target.textContent);
+    console.log(e);
+}));
